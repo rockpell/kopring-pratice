23ede73babc791c4bc742c5a27ad277ed8732402
rockpell <aythffk@gmail.com>
2024-01-08 23:48:24 +0900

    Chore: java 11->17, kotlin 1.5 -> 1.6

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.rockpell"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
